f0c1a402bd461ec575f6a3bc5e01df58fa8f1ab6
tiagosiebler <tiagosiebler@github.com>
2020-11-13 21:38:01 +0000

    move entrypoint

--- index.js
+++
@@ -1,9 +0,0 @@
-const RestClient = require('./lib/rest-client');
-const WebsocketClient = require('./lib/websocket-client');
-const DefaultLogger = require('./lib/logger');
-
-module.exports = {
-  RestClient,
-  WebsocketClient,
-  DefaultLogger
-};
