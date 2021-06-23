ef427f524af5acaa29905fc74977ac06977e6214
tiagosiebler <tiagosiebler@github.com>
2021-06-24 00:33:39 +0100

    detect handles

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   preset: "ts-jest",
   verbose: true, // report individual test
   bail: false, // enable to stop test when an error occur,
+  detectOpenHandles: true,
   moduleDirectories: ['node_modules', 'src', 'test'],
   testMatch: ['**/test/**/*.test.ts?(x)'],
   testPathIgnorePatterns: ['node_modules/', 'dist/', '.json'],
