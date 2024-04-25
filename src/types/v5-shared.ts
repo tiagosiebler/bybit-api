400e86a7777ab5fcf776a1a47eeb3fab238f4707
William <williamaggerhein@gmail.com>
2024-04-25 17:26:07 +0200

    Add `PositionStatusV5` and `PositionSideV5` types

--- src/types/v5-shared.ts
+++ src/types/v5-shared.ts
@@ -153,6 +153,16 @@ export type StopOrderTypeV5 =
  */
 export type PositionIdx = 0 | 1 | 2;
 
+/**
+ * Position status.
+ * 
+ * - 'Normal'
+ * - 'Liq' in the liquidation progress
+ * - 'Adl' in the auto-deleverage progress
+ */
+export type PositionStatusV5 = 'Normal' | 'Liq' | 'Adl';
+export type PositionSideV5 = 'Buy' | 'Sell';
+
 export type OptionTypeV5 = 'Call' | 'Put';
 
 /**
