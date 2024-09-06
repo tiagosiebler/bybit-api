bc318f0823274ef2ce49b4a43f0e10faa05a4903
Jerko J <83344666+JJ-Cro@users.noreply.github.com>
2024-09-06 10:14:02 +0200

    feat(): added missing API examples

--- examples/apidoc/V5/Account/get-transaction-log-classic.js
+++ examples/apidoc/V5/Account/get-transaction-log-classic.js
@@ -7,11 +7,9 @@ const client = new RestClientV5({
 });
 
 client
-  .getCoinBalance({
-    accountType: 'UNIFIED',
-    coin: 'USDT',
-    toAccountType: 'FUND',
-    withLtvTransferSafeAmount: 1,
+  .getClassicTransactionLogs({
+    limit: 1,
+    symbol: 'BTCUSD',
   })
   .then((response) => {
     console.log(response);
