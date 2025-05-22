9c592a2a48f2be4906ae78b6ef6343d387ab4e74
tiagosiebler <tiagosiebler@github.com>
2025-05-22 11:53:54 +0100

    chore(): update example doc

--- src/websocket-api-client.ts
+++ src/websocket-api-client.ts
@@ -47,7 +47,7 @@ export interface WSAPIClientConfigurableOptions {
  * may find the below methods slightly more intuitive.
  *
  * Refer to the WS API promises example for a more detailed example on using sendWSAPIRequest() directly:
- * https://github.com/tiagosiebler/bybit-api/blob/master/examples/ws-api-client.ts#L58
+ * https://github.com/tiagosiebler/bybit-api/blob/master/examples/ws-api-raw-promises.ts
  */
 export class WebsocketAPIClient {
   private wsClient: WebsocketClient;
