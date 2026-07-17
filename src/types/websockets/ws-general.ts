5183d9d78280c3c4f8e0a221d5c13da7b6ca8480
tiagosiebler <tiagosiebler@github.com>
2026-07-17 14:01:53 +0100

    fix: type regression

--- src/types/websockets/ws-general.ts
+++ src/types/websockets/ws-general.ts
@@ -126,7 +126,10 @@ export interface WSClientConfigurableOptions {
   wsOptions?: {
     protocols?: string[];
     agent?: any;
-  } & Partial<WebSocket.ClientOptions | ClientRequestArgs>;
+  } & (
+    | Omit<Partial<WebSocket.ClientOptions>, 'agent'>
+    | Omit<Partial<ClientRequestArgs>, 'agent'>
+  );
 
   wsUrl?: string;
 
